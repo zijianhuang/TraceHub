b8a0ba32fc1d556251f1a14c7cf8fe01c6c694e8
admin <admin@fonlow.com>
2016-08-19 10:19:44 +1000

    minor fix on bat

--- AuthDbCreator/CopyForCloudDev.bat
+++ AuthDbCreator/CopyForCloudDev.bat
@@ -1,5 +1,5 @@
 cd %~dp0
 set target=C:\Release\TraceHub\AuthDevCreatorCloudDev\
-xcopy bin\Debug\*.* %target% /Y /D
+xcopy bin\Release\*.* %target% /Y /D
 c:\green\XmlPreprocess\bin\XmlPreprocess.exe /i app.config /x ..\settings.xml /o %target%AuthDbCreator.exe.config /e MySql /clean
 pause
