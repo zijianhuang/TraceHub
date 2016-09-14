d651062726a8acdeea2beefddbc0dc16a250d1f6
admin <admin@fonlow.com>
2016-09-14 11:32:07 +1000

    clean up js scripts and use CDN

--- Fonlow.TraceHub/App_Start/BundleConfig.cs
+++ Fonlow.TraceHub/App_Start/BundleConfig.cs
@@ -8,21 +8,25 @@ namespace Fonlow.Web.Logging
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.UseCdn = true;
+            BundleTable.EnableOptimizations = true;
+            const string jqueryCdnPath = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
+            bundles.Add(new ScriptBundle("~/bundles/jquery", jqueryCdnPath).Include(
                         "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/bundles/modernizr", "http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.8.3.js").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js").Include(
+                      "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css").Include(
+                      "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/site.css").Include(
+                "~/Content/site.css"));
         }
     }
 }
