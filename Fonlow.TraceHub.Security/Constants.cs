66da39aec53b67f549e8d5a9cb816661dc201607
zijianhuang <admin@fonlow.com>
2017-05-15 08:03:59 +1000

    refactoring

--- Fonlow.TraceHub.Security/Constants.cs
+++ Fonlow.TraceHub.Security/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fonlow.TraceHub.Security
+﻿namespace Fonlow.TraceHub.Security
 {
     /// <summary>
     /// Roles used in operation contracts and the RoleProvider in ASP .NET
